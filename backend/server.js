49fbd3fdca955d6f2e63062c52b7405543b442ec
Diwakar-IT-Expert <diwakar.a2017@gmail.com>
2025-04-09 11:26:47 +0530

    first commit

---
+++ backend/server.js
@@ -0,0 +1,69 @@
+// const express = require("express")
+import path from "path";
+import express from "express"
+import dotenv from "dotenv"
+import cookieParser from "cookie-parser";
+import { v2 as cloudinary } from "cloudinary";
+import cors from "cors";
+
+import authRoute from "./routes/auth.route.js"
+import userRoutes from "./routes/user.route.js";
+import postRoutes from "./routes/post.route.js";
+import notificationRoutes from "./routes/notification.route.js";
+
+
+import connectDB from "./db/connectDB.js"; 
+
+dotenv.config();
+
+
+cloudinary.config({
+	cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+	api_key: process.env.CLOUDINARY_API_KEY,
+	api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+
+const app = express()
+const __dirname = path.resolve();
+const PORT = process.env.PORT;
+
+app.use(cors({
+    origin: "http://localhost:5173",  // allow the frontend to access the backend
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true,  // Allow cookies and credentials to be sent
+   
+}));
+
+app.use(express.json({ limit: "5mb" })); // to parse req.body
+// limit shouldn't be too high to prevent DOS
+app.use(express.urlencoded({ extended: true })); // to parse form data(urlencoded)
+
+
+
+// app.use(express.json());
+app.use(cookieParser());
+
+app.use("/api/auth", authRoute);
+app.use("/api/users", userRoutes);
+app.use("/api/posts", postRoutes);
+app.use("/api/notifications", notificationRoutes);
+
+
+
+// app.get("/", (req, res) => {
+//     res.send("Hello Hi")
+// })
+
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+	});
+}
+
+app.listen(PORT, () => {
+    console.log(`Server started in ${PORT}`)
+    connectDB();
+})
---
+++ frontend/src/App.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import { Navigate, Route, Routes } from 'react-router-dom';
+import HomePage from './pages/home/HomePage';
+import SignUpPage from './pages/auth/signup/SignUpPage';
+import LoginPage from './pages/auth/login/LoginPage';
+import Sidebar from './components/common/Sidebar';
+import RightPanel from './components/common/RightPanel';
+import NotificationPage from './pages/notification/NotificationPage';
+import ProfilePage from './pages/profile/ProfilePage';
+import { Toaster } from "react-hot-toast";
+import LoadingSpinner from './components/common/LoadingSpinner';
+import useAuthUser from './hooks/useAuthUser';
+
+
+
+const App = () => {
+
+	const { data: authUser, isLoading } =  useAuthUser();
+	
+	if (isLoading) {
+		return (
+			<div className='h-screen flex justify-center items-center'>
+				<LoadingSpinner size='lg' />
+			</div>
+		);
+	}
+
+	return (
+		<div className='flex max-w-6xl mx-auto'>
+			{authUser && <Sidebar />}
+			<Routes>
+				<Route path='/' element={authUser ? <HomePage /> : <Navigate to='/login' />} />
+				<Route path='/login' element={!authUser ? <LoginPage /> : <Navigate to='/' />} />
+				<Route path='/signup' element={!authUser ? <SignUpPage /> : <Navigate to='/' />} />
+				<Route path='/notifications' element={authUser ? <NotificationPage /> : <Navigate to='/login' />} />
+				<Route path='/profile/:username' element={authUser ? <ProfilePage /> : <Navigate to='/login' />} />
+			</Routes>
+			{authUser && <RightPanel />}
+			<Toaster />
+		</div>
+	);
+}
+
+export default App
